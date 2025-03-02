66176e220aeb035422951ca56064c006d1458ced
moth <dinocruz434@gmail.com>
2025-03-02 18:35:26 +0000

    loading and saving locally stored tasks
    - tasks are saved as a multidimensional array

--- storage_manager.py
+++ storage_manager.py
@@ -1,11 +1,12 @@
 import flet as ft
+from todo_app import Task
 
 class StorageManager:
     def __init__(self, page: ft.Page):
         self.page = page
     
-    def check_storage(self, e):
-        storage = self.page.client_storage.get("key")
+    def check_storage(self):
+        storage = self.page.client_storage.get("tasks")
 
         if storage:
             self.load_storage(storage)
@@ -13,5 +14,19 @@ class StorageManager:
             return None
 
     def load_storage(self, storage):
-        # Implement the logic to load storage here
-        pass
+        tasks = storage
+
+        for task_data in tasks:
+            task_name, task_completed = task_data
+            task = Task(task_name, self.page.task_status_change, self.page.task_delete)
+            task.completed = task_completed
+            task.display_task.value = task_completed
+            self.page.tasks.controls.append(task)
+
+        self.page.update()
+
+    def save_storage(self):
+        tasks = []
+        for task in self.page.tasks.controls:
+            tasks.append((task.task_name, task.completed))
+        self.page.client_storage.set("tasks", tasks)
